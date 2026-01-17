6f7986af56fc01a7ae9261d41f2860e5e8c0e657
Luca Porto Duarte <lduarte@uwo.ca>
2026-01-17 13:34:54 -0500

    quick fix

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { Amplitude } from '@/amplitude';
-import Providers from './AmplitudeProvider';
 import "./globals.css";
 
 const geistSans = Geist({
