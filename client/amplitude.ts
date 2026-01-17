9cd75e472b031ab347c6915bb6693d9ac2bc1355
Luca Porto Duarte <lduarte@uwo.ca>
2026-01-16 23:00:31 -0500

    testing session replay

--- client/amplitude.ts
+++ client/amplitude.ts
@@ -6,7 +6,10 @@ import { sessionReplayPlugin } from '@amplitude/plugin-session-replay-browser';
 
 function initAmplitude() {
   if (typeof window !== 'undefined') {
-    amplitude.add(sessionReplayPlugin());
+    amplitude.add(sessionReplayPlugin({
+        forceSessionTracking: true,
+        sampleRate: 1
+    }));
     amplitude.init('cd691afe1bca5ab691835fd287e85893', {"autocapture":true});
   }
 }
